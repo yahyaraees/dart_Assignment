96f7a3c5cf4946a5cda3db5a5db5e3647f0836f7
yahyaraees <yahyaraees667@gmail.com>
2025-05-24 20:10:21 +0500

    update

--- app.dart
+++ app.dart
@@ -3,10 +3,10 @@ void main() {
 // assign values then check if they are square values or rectangle values.
 // ie: if both values are equal then it's square otherwise rectangle.
 
-print("jkfjldjfld`")
-print("jkfjldjfld`")
-print("jkfjldjfld`")
-print("jkfjldjfld`")
-print("jkfjldjfld`")
+print("jkfjldjfld`");
+print("jkfjldjfld`");
+print("jkfjldjfld`");
+print("jkfjldjfld`");
+print("jkfjldjfld`");
 
 }
