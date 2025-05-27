beeb711b5a15c8908e712a6ff6f798d71ade4d4f
yahyaraees <yahyaraees667@gmail.com>
2025-05-28 00:31:06 +0500

    update

--- app.dart
+++ app.dart
@@ -1,18 +1,56 @@
-// 
 
 void main(){
+  ////////////// Qno1 //////////////
+//   int lenght = 8;
+//   int breath = 9;
 
-  int lenght = 8;
-  int breath = 9;
+//  if  (lenght == breath){
+//     print('both are same value ');
+//  }
+//  else{
+//     print('not equal');
+//  } 
 
- if  (lenght == breath){
-    print('both are same value ');
- }
- else{
-    print('not equal');
-    print('not equal');
-    print('not equal');
-    print('not equal');
- } 
+  /////////// Qno2 /////////////
+// var number1 = 1;
+// var number2 = 2;
+
+
+// if (number1 > number2){
+//   print('number 1 is big');  
+//   print('number 2 is small');   
+// }
+// else if( number2 > number1){
+//  print('number 2 is big');
+//  print('number 1 is small');
+// }
+// else{
+//    print('both are same');
+// }
+
+
+
+var held = 20;
+var attend = 19;
+
+var final_output = (attend/held)* 100;
+
+if (final_output > 75 ) {
+   print('you are elligible'); 
+}
+else{
+   print('you are not elligible');
+}
+print(final_output); 
+
+
+
+
+
+
+
+
 
 }
+
+
